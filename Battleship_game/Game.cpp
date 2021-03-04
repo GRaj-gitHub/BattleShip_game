d6a0973bf93234f8c335a094ff4d035ddf1c4f08
GRaj-gitHub <rajgopika1105@gmail.com>
2021-03-04 20:53:44 +0000

    changing no of ships

--- Battleship_game/Game.cpp
+++ Battleship_game/Game.cpp
@@ -6,27 +6,15 @@ int battle_ground[10][10];
 char display_ground[10][10];
 int ship_size = 5;
 int destroyer_size = 2;
+bool game_over = false;
 
 
-void test()
-{
-	cout << endl << "testing" << endl;
 
-	for (int i = 0; i < 10; i++)
-	{
-		for (int j = 0; j < 10; j++)
-		{
-			cout << battle_ground[i][j] << " ";
-		}
-		cout << endl;
-	}
-
-}
 int main()
 {
-	bool game_over = false;
-	int row_input;
-	int col_input;
+	
+	int row_input, col_input;
+	int count, winvalue;
 	for (int i = 0; i < 10; i++)
 	{
 		for (int j = 0; j < 10; j++)
@@ -36,7 +24,15 @@ int main()
 		}
 	}
 	srand((unsigned)time(0));
-	ship_generator();
+	for (int i = 0; i < 2; i++)
+	{
+		ship_generator();
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		des_gen();
+	}
+	
 	while (game_over == false)
 	{
 		cout << "enter row" << endl;
@@ -44,8 +40,12 @@ int main()
 		cout << endl << "enter col" << endl;
 		cin >> col_input;
 		cout << endl;
-		display_field(&row_input, &col_input);
-	}
+		display_field(&row_input, &col_input, &count);
+		if (count == 18)
+		{
+			cout << endl << "YOU WIN !!";
+		}
+	} 
 	
 	
 	return 0;
@@ -53,41 +53,74 @@ int main()
 
 void ship_generator()
 {
-	
+	bool flag = false;
 	int s_row = rand() % 6;
 	int s_col = rand() % 6 ;
 	int choice = rand() % 2;// randomly choose to place row wise or coloumn wise;
 	if (choice == 0)
 	{
+		int temp = s_row;
 		for (int i = 0; i < ship_size; i++)
 		{
-			battle_ground[s_row][s_col] = 1;
-			s_col++;
-
+			if (battle_ground[temp][s_col] != 0)
+			{
+				flag = true;
+				break;
+			}
+			temp++;
 		}
+		if (!flag)
+		{
+			for (int i = 0; i < ship_size; i++)
+			{
+				battle_ground[s_row][s_col] = 1;
+				s_row++;
+
+			}
+		}
+		else
+		{
+			ship_generator();
+		}
+		
 		
 	}
 
 	else
 	{
+		int temp = s_col;
 		for (int i = 0; i < ship_size; i++)
 		{
-			battle_ground[s_row][s_col] = 1;
-			s_row++;
+			if (battle_ground[s_row][temp] != 0)
+			{
+				flag = true;
+				break;
+			}
+			temp++;
 		}
+		if (!flag)
+		{
+			for (int i = 0; i < ship_size; i++)
+			{
+				battle_ground[s_row][s_col] = 1;
+				s_col++;
+			}
+		}
+		else
+		{
+			ship_generator();
+		}
+		
 	}
-	for (int count = 0; count < 4; count++)
-	{
-		des_gen();
-	}
-	test();
+	
+	
 	
 }
 
 void des_gen()
 {
 	
-	int flag = 0;
+	bool flag = false;
 	int s_row = rand() % 8;
 	int s_col = rand() % 8;
 	int choice = rand() % 2;// randomly choose to place row wise or coloumn wise;
@@ -95,25 +128,23 @@ void des_gen()
 	
 	if (choice == 0)
 	{
+		int temp = s_row;
 		for (int i = 0; i < destroyer_size; i++)
 		{
-			for (int j = 0; j < destroyer_size; j++)
+			if (battle_ground[temp][s_col] != 0)
 			{
-				if (battle_ground[s_row][s_col] != 0)
-				{
-					flag = 1;
-				}
-				s_col++;
+				flag = true;
+				break;
 			}
-			s_row++;
+			temp++;
 		}
 		
-		if (flag == 0)
+		if (!flag)
 		{
 			for (int i = 0; i < destroyer_size; i++)
 			{
 				battle_ground[s_row][s_col] = 2;
-				s_col++;
+				s_row++;
 
 			}
 			
@@ -126,25 +157,23 @@ void des_gen()
 	}
 	else
 	{
+		int temp = s_col;
 		for (int i = 0; i < destroyer_size; i++)
 		{
-			for (int j = 0; j < destroyer_size; j++)
+			if (battle_ground[s_row][temp] != 0)
 			{
-				if (battle_ground[s_row][s_col] != 0)
-				{
-					flag = 1;
-				}
-				s_row++;
+				flag = true;
+				break;
 			}
-			s_col++;
+			temp++;
 		}
 		
-		if (flag == 0)
+		if (!flag)
 		{
 			for (int i = 0; i < destroyer_size; i++)
 			{
 				battle_ground[s_row][s_col] = 2;
-				s_row++;
+				s_col++;
 
 			}
 			
@@ -157,24 +186,34 @@ void des_gen()
 
 }
 
-void display_field(int* row_input, int* col_input)
+int display_field(int* row_input, int* col_input, int* count)
 {
+
+	if (battle_ground[*row_input][*col_input] == 0)
+	{
+		display_ground[*row_input][*col_input] = 'O';
+		
+	}
 	if (battle_ground[*row_input][*col_input] == 1)
 	{
 		display_ground[*row_input][*col_input] = 'S';
+		*count++;
 	}
 	else if (battle_ground[*row_input][*col_input] == 2)
 	{
 		display_ground[*row_input][*col_input] = 'D';
+		*count++;
 	}
 	for (int i = 0; i < 10; i++)
 	{
 		for (int j = 0; j < 10; j++)
 		{
 
-			cout << display_ground[i][j] <<" ";
+			cout << display_ground[i][j] <<"  ";
 		}
 		cout << endl;
 	}
+	return *count;
+	
 
 }
--- Battleship_game/Game.h
+++ Battleship_game/Game.h
@@ -1,4 +1,4 @@
 #pragma once
 void ship_generator();
-void display_field(int* row_input, int* col_input);
+int display_field(int* row_input, int* col_input, int* count);
 void des_gen();
